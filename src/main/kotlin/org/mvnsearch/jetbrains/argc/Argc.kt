3e1f28b9810d74fbaebb33adc4b2b7238f059992
Libing Chen <libing.chen@gmail.com>
2024-08-18 15:33:53 +0800

    app: Argc

---
+++ src/main/kotlin/org/mvnsearch/jetbrains/argc/Argc.kt
@@ -0,0 +1,58 @@
+package org.mvnsearch.jetbrains.argc
+
+val ALL_TAGS = listOf("flag", "option", "arg", "describe", "cmd", "describe", "meta", "env", "alias")
+
+
+data class ArgcArgument(val type: String, val shortName: String?, val longName: String?, val description: String?)
+
+fun parseArgcComment(scriptText: String): List<ArgcArgument> {
+    return scriptText.lines()
+        .asSequence()
+        .filter { it.startsWith("# @") }
+        .map { it.substring(3) }
+        .filter { it.startsWith("option") || it.startsWith("flag") || it.startsWith("arg") }
+        .map { commentTag ->
+            val parts = commentTag.split(" ")
+            val type = parts[0]
+            var shortName: String? = null
+            var longName: String? = null
+            var description: String? = null
+            var descOffset = 1
+            for (i in 1 until parts.size) {
+                if (!(parts[i].startsWith("-")
+                            || parts[i].startsWith("+")
+                            || parts[i].startsWith("<"))
+                ) {
+                    descOffset = i
+                    break
+                }
+            }
+            if (descOffset < parts.size) {
+                description = parts.subList(descOffset, parts.size).joinToString(" ")
+            }
+            if (descOffset > 1) {
+                for (i in 1 until descOffset) {
+                    val part = parts[i]
+                    if(part.startsWith("-") || part.startsWith("+")) {
+                        var name = if (part.startsWith("--")) {
+                            parts[i].substring(2)
+                        } else {
+                            part.substring(1)
+                        }
+                        val offset = name.indexOfFirst { !it.isLetterOrDigit() }
+                        if (offset > 0) {
+                            name = name.substring(offset)
+                        }
+                        if (name.length == 1) {
+                            shortName = name
+                        } else {
+                            longName = name
+                        }
+                    }
+                }
+            }
+            ArgcArgument(type, shortName, longName, description)
+        }
+        .filter { it.shortName != null || it.longName != null }
+        .toList()
+}
